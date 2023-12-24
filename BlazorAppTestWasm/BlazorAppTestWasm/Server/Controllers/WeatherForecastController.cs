5fb9ff04a33d45e23bc481f7bba08c31d10348b5
jihun <jihun315016@gmail.com>
2023-12-24 17:20:02 +0900

    init

---
+++ BlazorAppTestWasm/BlazorAppTestWasm/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using BlazorAppTestWasm.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAppTestWasm.Server.Controllers
+{
+    // [Route("[controller]")]
+    // 컨트롤러 앞에 있는 이름(WeatherForecast)으로 지정을 하면 자동으로 이 컨트롤러로 라우팅을 시키겠다는 것
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
