0fee553aecf643689e08010db3945ad563484d25
jihun <jihun315016@gmail.com>
2024-01-01 19:39:13 +0900

    Blazor EF Core CRUD 예제

--- BlazorAppWasmTest/BlazorAppWasmTest/Server/Controllers/EmployeeController.cs
+++ BlazorAppWasmTest/BlazorAppWasmTest/Server/Controllers/EmployeeController.cs
@@ -11,6 +11,14 @@ namespace BlazorAppWasmTest.Server.Controllers
     {
         private PostgresContext _postgresContext = new PostgresContext();
 
+        [HttpGet]
+        [Route("read")]
+        public IEnumerable<Employee> GetEmployees()
+        {
+            // 테이블에 있는 데이터 리턴
+            return this._postgresContext.Employees.ToList();
+        }
+
         [HttpPost]
         [Route("insert")]
         public async Task<IActionResult> Post([FromBody] Employee employee)
@@ -32,6 +40,54 @@ namespace BlazorAppWasmTest.Server.Controllers
             catch (Exception ex)
             {
                 // 500은 인터널 서버 에러
+                return StatusCode(500, ex.Message);
+            }
+        }
+       
+        [HttpPut]
+        [Route("update")]
+        public async Task<IActionResult> Update([FromBody] Employee employee)
+        {
+            // 서버쪽에서도 받을때 body로부터 객체를 꺼내와야 함 -> [FromBody]
+
+            // 업데이트니까 null 체크
+            if (employee == null) 
+            {
+                return BadRequest();
+            }
+            
+            try
+            {
+                this._postgresContext.Update(employee);
+                this._postgresContext.SaveChanges();
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var employee = await _postgresContext.Employees.FindAsync(id);
+            if (employee == null) 
+            {
+                return NotFound();
+            }
+
+
+            try
+            {
+                this._postgresContext.Remove(employee);
+                this._postgresContext.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
                 return StatusCode(500, ex.Message);
             }
         }
