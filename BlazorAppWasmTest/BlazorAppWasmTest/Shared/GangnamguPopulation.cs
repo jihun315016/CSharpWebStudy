7e49f8ba43eb4122021beb48822ea136d0daf44d
jihun <jihun315016@gmail.com>
2024-01-03 21:56:51 +0900

    PostgreSQL DB 복원

---
+++ BlazorAppWasmTest/BlazorAppWasmTest/Shared/GangnamguPopulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorAppWasmTest.Shared;
+
+public partial class GangnamguPopulation
+{
+    public string? AdministrativeAgency { get; set; }
+
+    public int? TotalPopulation { get; set; }
+
+    public int? MalePopulation { get; set; }
+
+    public int? FemalePopulation { get; set; }
+
+    public double? SexRatio { get; set; }
+
+    public int? NumberOfHouseholds { get; set; }
+
+    public double? NumberOfPeoplePerHousehold { get; set; }
+
+    public int Id { get; set; }
+}
