83f5f7b66f00a039a46c37eaa24ddb00eed89f0c
jihun <jihun315016@gamil.com>
2024-01-08 17:39:26 +0900

    .NET 8.0에서 Blozor 관련 실습 - 임시 솔루션

---
+++ Solution1/ClassLibrary1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Hobby { get; set; }
+}
