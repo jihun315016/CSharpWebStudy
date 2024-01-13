60d8ba5361d68a9bdd69699a8ccc663177e97577
jihun <jihun315016@gmail.com>
2024-01-13 14:15:42 +0900

    BlazorAppWasmTest개발 구조 세팅

---
+++ BlazorAppWasmTest/BlazorAppWasmTest/Server/Controllers/GangnamguPopulationController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorAppWasmTest.Server.Interfaces;
+using BlazorAppWasmTest.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAppWasmTest.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GangnamguPopulationController : ControllerBase
+    {
+        // 아까 인터페이스를 했으니까 이 컨트롤러에서는 인터페이스를 통해서 함수를 호출하면 된다.
+        // 그래서 인터페이스 타입 객체를 만들어준다.
+        private readonly IGangnamguPopulation _IGgangnamguPopulation;
+
+        public GangnamguPopulationController(IGangnamguPopulation iGangnamguPopulation)
+        {
+            _IGgangnamguPopulation = iGangnamguPopulation;
+        }
+
+        [HttpGet]
+        public IEnumerable<GangnamguPopulation> Get()
+        {
+            // API에서 GET을 호출하면 인터페이스를 타고가서 실제 기능 수행은 서비스 클래스쪽에서 하게 된다.
+            return _IGgangnamguPopulation.GetAllGangnamguPopulations();
+        }
+    }
+}
---
+++ BlazorAppWasmTest/BlazorAppWasmTest/Server/Interfaces/IGangnamguPopulation.cs
@@ -0,0 +1,18 @@
+﻿using BlazorAppWasmTest.Shared;
+
+namespace BlazorAppWasmTest.Server.Interfaces
+{
+    public interface IGangnamguPopulation
+    {
+        public void AddGangnamguPopulation(GangnamguPopulation population);
+
+        public void UpdateGangnamguPopulation(GangnamguPopulation population);
+
+        // 파라미터 -> 원래는 id 값이 있어야 하는데 테이블에 id 값이 없어서 이걸로 대체
+        public void DeleteGangnamguPopulation(string agency);
+
+        public GangnamguPopulation GetGangnamguPopulation(string agency);
+
+        public List<GangnamguPopulation> GetAllGangnamguPopulations();
+    }
+}
